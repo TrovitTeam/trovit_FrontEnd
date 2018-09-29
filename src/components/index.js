01f0cac6b73f4f5ee5ee4a7eb994a073e0aa7fca
miacastroco <miacastroco@unal.edu.co>
2018-09-29 17:41:29 -0500

    Organization of components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from '../registerServiceWorker';
 import {BrowserRouter, Route} from 'react-router-dom'
 
 ReactDOM.render(
