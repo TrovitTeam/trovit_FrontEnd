1954c273e1c5226ed628acd5a3ee0531360efae3
Arka Ariza <arjarizami@unal.edu.co>
2018-09-29 17:16:57 -0500

    index routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import App from './js/App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route path = '/' component = {App}/>
+            <Route path = '/' component = {DogsList}/>
+            <Route path = '/' component = {AuthorCreation}/>
+        </div>
+    </BrowserRouter>
+    , document.getElementById('root'));
+            
 registerServiceWorker();
