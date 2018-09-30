0bd4750e9d228cb1f88f2918c920027ae23cdfb0
miacastroco <miacastroco@unal.edu.co>
2018-09-30 13:42:25 -0500

    Sign In changes

--- src/components/Sign_in.js
+++ src/components/Sign_in.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import {Input, Row, Icon, Button} from 'react-materialize'
+import {Input, Row, Icon, Button, Col} from 'react-materialize'
+import glogo from "../resources/glogo.svg"
+import flogo from "../resources/flogo.svg"
+import gmlogo from "../resources/gmail.svg"
 
 
 class Sign_in extends Component {
@@ -7,14 +10,34 @@ class Sign_in extends Component {
     return (
       <div class="container">
         <Row>
+          <blockquote>
+            <h1 class="center">Sign In</h1>
+          </blockquote>
+        </Row>
+        <Row>
             <Input s={6} label="First Name" validate><Icon>account_circle</Icon></Input>
-            <Input s={6} label="Last Name" />
+            <Input s={6} label="Last Name"/>
             <Input s={12} type="password" label="password" validate><Icon>fingerprint</Icon></Input>
             <Input s={12} type="email" label="email" validate><Icon>email</Icon></Input>
         </Row>
-        <div>
-            <Button waves='light'>Google<Icon left>cloud</Icon></Button>
-            <Button waves='light'>Facebook<Icon left>cloud</Icon></Button>
+        <div class="center">
+          <Button className="light-blue darken-4" waves='light'>Register</Button>
+        </div>
+        <div class="center">
+          <Row>
+            <Col s={4}>
+              <p class="flow-text">Google +</p>
+              <img width="10%" src={glogo}/>
+            </Col>
+            <Col s={4}>   
+              <p class="flow-text">Facebook</p>
+              <img width="10%" src={flogo}/>
+            </Col>
+            <Col s={4}>
+              <p class="flow-text">Gmail</p>
+              <img width="10%" src={gmlogo}/>
+            </Col>
+          </Row>
         </div>
       </div>
     );
