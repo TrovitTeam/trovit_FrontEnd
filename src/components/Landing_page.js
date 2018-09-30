f5e29c50868bec20333a3621fba5b57272d5d8d5
miacastroco <miacastroco@unal.edu.co>
2018-09-30 00:09:11 -0500

    Landing Page Minor Adjustments

--- src/components/Landing_page.js
+++ src/components/Landing_page.js
@@ -17,107 +17,99 @@ class Landing_page extends Component {
       <div class="container">
           <Slider {...settings}>
             <div class="container">
-              <img width="100%" height="200 px"src="http://www.grupoduplex.com/wp-content/uploads/2018/09/H71626735-Khaki-Field-Auto-Chrono-in-Jack-Ryan-3-6000x500.jpg" />
+              <img width="100%" src="https://www.annarborartcenter.org/wp-content/uploads/2017/03/Text-and-Image-Ann-Arbor-Art-center-117-Gallery-Exhibition-Ann-Arbor-Gallery.png" />
             </div>
             <div>
-              <img width="100%" height="200 px" src="http://xn--schnstegeschenke-owb.de/wp-content/uploads/2018/07/kopfh%C3%B6rer_geschenke_m%C3%A4nner-e1531051063523-6000x500.jpg" />
+              <img width="100%" src="https://www.annarborartcenter.org/wp-content/uploads/2017/03/Text-and-Image-Ann-Arbor-Art-center-117-Gallery-Exhibition-Ann-Arbor-Gallery.png" />
             </div>
             <div>
-              <img width="100%" height="200 px" src="http://xn--schnstegeschenke-owb.de/wp-content/uploads/2018/07/brettspiele_geschenke_m%C3%A4nner-6000x500.jpg" />
+              <img width="100%" src="https://www.annarborartcenter.org/wp-content/uploads/2017/03/Text-and-Image-Ann-Arbor-Art-center-117-Gallery-Exhibition-Ann-Arbor-Gallery.png" />
             </div>
             <div>
-              <img width="100%" height="200 px" src="http://www.grupoduplex.com/wp-content/uploads/2018/09/H71626735-Khaki-Field-Auto-Chrono-in-Jack-Ryan-3-6000x500.jpg" />
+              <img width="100%" src="https://www.annarborartcenter.org/wp-content/uploads/2017/03/Text-and-Image-Ann-Arbor-Art-center-117-Gallery-Exhibition-Ann-Arbor-Gallery.png" />
             </div>
             <div>
-              <img width="100%" height="200 px" src="http://xn--schnstegeschenke-owb.de/wp-content/uploads/2018/07/kopfh%C3%B6rer_geschenke_m%C3%A4nner-e1531051063523-6000x500.jpg" />
+              <img width="100%" src="https://www.annarborartcenter.org/wp-content/uploads/2017/03/Text-and-Image-Ann-Arbor-Art-center-117-Gallery-Exhibition-Ann-Arbor-Gallery.png" />
             </div>
             <div>
-              <img width="100%" height="200 px" src="http://xn--schnstegeschenke-owb.de/wp-content/uploads/2018/07/brettspiele_geschenke_m%C3%A4nner-6000x500.jpg" />
+              <img width="100%" src="https://www.annarborartcenter.org/wp-content/uploads/2017/03/Text-and-Image-Ann-Arbor-Art-center-117-Gallery-Exhibition-Ann-Arbor-Gallery.png" />
             </div>
           </Slider>
           <Row>
-              <Col s={6} m={3}>
+              <Col s={7} m={4}>
                   <div>
                     <ul class="collection">
                       <li class="collection-item avatar">
-                        <i class="material-icons circle">folder</i>
-                        <span class="title">Title</span>
-                        <p>First Line <br></br>
-                            Second Line
+                        <i class="material-icons circle">category</i>
+                        <span class="title flow-text">Categories</span>
+                        <p>
+                            Games <br></br>
+                            Tools <br></br>
+                            Phones<br></br>
+                            Sports <br></br>
+                            Electronics<br></br>
+                            Clothes<br></br>
+                            Computation <br></br>
                         </p>
-                        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                        <div>{[<a href='#'>More</a>]}</div>
                       </li>
                       <li class="collection-item avatar">
-                        <i class="material-icons circle">folder</i>
-                        <span class="title">Title</span>
-                        <p>First Line <br></br>
-                            Second Line
+                        <i class="material-icons circle">location_city</i>
+                        <span class="title flow-text">City</span>
+                        <p> Bogotá D.C. <br></br>
+                            Cundinamarca <br></br>
+                            Atlantico <br></br>
+                            Antioquia <br></br>
+                            Valle Del Cauca <br></br>
+                            Magdalena <br></br>
+                            Santander <br></br>
+                            Caldas <br></br>
+                            Nariño <br></br>
+                            Risaralda <br></br>
                         </p>
-                        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                        <div>{[<a href='#'>More</a>]}</div>
                       </li>
                       <li class="collection-item avatar">
-                        <i class="material-icons circle">folder</i>
-                        <span class="title">Title</span>
-                        <p>First Line <br></br>
-                            Second Line
+                        <i class="material-icons circle">local_shipping</i>
+                        <span class="title flow-text">Shipping</span>
+                        <p>Free Shipping<br></br>
+                           Inter-Regional Free Shipping
                         </p>
-                        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                        <div>{[<a href='#'>More</a>]}</div>
                       </li>
                       <li class="collection-item avatar">
-                        <i class="material-icons circle">folder</i>
-                        <span class="title">Title</span>
-                        <p>First Line <br></br>
-                            Second Line
+                        <i class="material-icons circle">attach_money</i>
+                        <span class="title flow-text">Price (COP)</span>
+                        <p>Less than $50000.0 <br></br>
+                            $51000.00 - $500000.0 <br></br>
+                            More than $500000.0
                         </p>
-                        <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+                        <div>{[<a href='#'>More</a>]}</div>
                       </li>
                     </ul>
                   </div>
               </Col>
-              <Col s={6} m={9}>
+              <Col s={5} m={8}>
                 <Table>
                   <tbody>
                     <tr>
                       <td>
                         <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
                           actions={[<a href='#'>Product Link</a>]}>
                           Product Description.
                         </Card>
                       </td>
                       <td>
                         <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
                           actions={[<a href='#'>Product Link</a>]}>
                           Product Description.
                         </Card>
                       </td>
                       <td>
                         <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
-                          actions={[<a href='#'>Product Link</a>]}>
-                          Product Description.
-                        </Card>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>
-                        <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
-                          actions={[<a href='#'>Product Link</a>]}>
-                          Product Description.
-                        </Card>
-                      </td>
-                      <td>
-                        <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
-                          actions={[<a href='#'>Product Link</a>]}>
-                          Product Description.
-                        </Card>
-                      </td>
-                      <td>
-                        <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
                           actions={[<a href='#'>Product Link</a>]}>
                           Product Description.
                         </Card>
@@ -126,21 +118,44 @@ class Landing_page extends Component {
                     <tr>
                       <td>
                         <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
                           actions={[<a href='#'>Product Link</a>]}>
                           Product Description.
                         </Card>
                       </td>
                       <td>
                         <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
                           actions={[<a href='#'>Product Link</a>]}>
                           Product Description.
                         </Card>
                       </td>
                       <td>
                         <Card className='small blue-grey'
-                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Card Title</CardTitle>}
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
+                          actions={[<a href='#'>Product Link</a>]}>
+                          Product Description.
+                        </Card>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <Card className='small blue-grey'
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
+                          actions={[<a href='#'>Product Link</a>]}>
+                          Product Description.
+                        </Card>
+                      </td>
+                      <td>
+                        <Card className='small blue-grey'
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
+                          actions={[<a href='#'>Product Link</a>]}>
+                          Product Description.
+                        </Card>
+                      </td>
+                      <td>
+                        <Card className='small blue-grey'
+                          header={<CardTitle image="http://mikemoir.com/mikemoir/wp-content/uploads/2015/06/MedRes_Product-presentation-2.jpg">Product Title</CardTitle>}
                           actions={[<a href='#'>Product Link</a>]}>
                           Product Description.
                         </Card>
