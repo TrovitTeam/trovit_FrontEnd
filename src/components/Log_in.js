12f99ffdbe050afada58610657fb7c068898a527
Arka Ariza <arjarizami@unal.edu.co>
2018-10-15 23:22:45 -0500

    LogIn config

--- src/components/Log_in.js
+++ src/components/Log_in.js
@@ -3,8 +3,59 @@ import {Input, Row, Icon, Button, Col} from 'react-materialize'
 import glogo from "../resources/glogo.svg"
 import flogo from "../resources/flogo.svg"
 import gmlogo from "../resources/gmail.svg"
+import axios from 'axios';
+
 
 class Log_in extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+  };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+
+    this.setState({
+      [name] : value
+    });
+
+    
+  }
+  
+  handleSubmit(event){
+      var _this = this;
+      axios({
+          method:'post',
+          url:'http://localhost:3000/user_token',
+          responseType: "json",
+          data: {
+            "auth":
+            {
+              "email": _this.state.email,
+              "password": _this.state.password
+            }
+          }
+      })
+      .then(function(response) {
+
+          console.log(response);
+      })
+      .catch(function (error) {
+      console.log(error);
+
+      });
+  }
+  
   render() {
     return (
       <div className="container">
@@ -14,11 +65,11 @@ class Log_in extends Component {
           </blockquote>
         </Row>
         <Row>
-          <Input s={12} type="email" label="email" validate data-length="45"><Icon>email</Icon></Input>
-          <Input s={12} type="password" label="password" validate data-length="45"><Icon>fingerprint</Icon></Input>  
+          <Input s={12} name="email" type="email" label="email" value={this.state.email} onChange={this.handleChange} validate data-length="45"><Icon>email</Icon></Input>
+          <Input s={12} name="password" type="password" label="password" value={this.state.password} onChange={this.handleChange} validate data-length="45"><Icon>fingerprint</Icon></Input>  
         </Row>
         <div className="center">
-          <Button className="light-blue darken-4" waves='light'>Log In</Button>
+          <Button className="light-blue darken-4" waves='light' onClick={this.handleSubmit}>Log In</Button>
         </div>
         <div className="center">
           <Row>
