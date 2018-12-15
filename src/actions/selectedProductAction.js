e7610ea7be6bc971744c432a3614fc7a056452e0
Arka Ariza <arjarizami@unal.edu.co>
2018-12-15 17:54:00 -0500

    Update selectedProductAction.js

--- src/actions/selectedProductAction.js
+++ src/actions/selectedProductAction.js
@@ -3,7 +3,7 @@ import unsplash from "../apis/unsplash";
 export const fetchProductInfo = id => async dispatch => {
 	const response = await unsplash.get(`/photos/${id}`);
 
-	console.log("hey", response);
+	console.log("hey", response.data);
 
 	dispatch({
 		type: "SELECT_PRODUCT",
