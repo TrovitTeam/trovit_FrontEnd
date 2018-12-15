5ad0a1d4e338eef264d1ca40a26d7946714b8cd8
miacastroco <miacastroco@unal.edu.co>
2018-12-15 13:31:36 -0500

    SearchList Proxy

--- src/components/Search_result.js
+++ src/components/Search_result.js
@@ -1,69 +1,31 @@
 import React, { Component } from 'react';
-import {Col, Row, Pagination} from 'react-materialize'
+import { connect } from 'react-redux';
+import searchAction from '../actions/searchAction';
+import {Card} from 'react-materialize'
 
 class Search_result extends Component {
+
+  componentDidMount = () => {
+    this.props.searchAction();
+  }
+
+  renderList = () => {
+    return this.props.searchAction.map( act => {
+      return(
+        <Card className='small blue-grey'
+          header={<CardTitle image={src02}>Product Title</CardTitle>}
+          actions={[<a href='/'>Product Link</a>]}>
+          Product Description.
+        </Card>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="container">
-            <Row>
-                <Col s={7} m={4}>
-                    <div>
-                      <ul className="collection">
-                        <li className="collection-item avatar">
-                          <i className="material-icons circle">category</i>
-                          <span className="title flow-text">Categories</span>
-                          <p>
-                              Games <br></br>
-                              Tools <br></br>
-                              Phones<br></br>
-                              Sports <br></br>
-                              Electronics<br></br>
-                              Clothes<br></br>
-                              Computation <br></br>
-                          </p>
-                          <div>{[<a href='/'>More</a>]}</div>
-                        </li>
-                        <li className="collection-item avatar">
-                          <i className="material-icons circle">location_city</i>
-                          <span className="title flow-text">City</span>
-                          <p> Bogotá D.C. <br></br>
-                              Cundinamarca <br></br>
-                              Atlantico <br></br>
-                              Antioquia <br></br>
-                              Valle Del Cauca <br></br>
-                              Magdalena <br></br>
-                              Santander <br></br>
-                              Caldas <br></br>
-                              Nariño <br></br>
-                              Risaralda <br></br>
-                          </p>
-                          <div>{[<a href='/'>More</a>]}</div>
-                        </li>
-                        <li className="collection-item avatar">
-                          <i className="material-icons circle">local_shipping</i>
-                          <span className="title flow-text">Shipping</span>
-                          <p>Free Shipping<br></br>
-                            Inter-Regional Free Shipping
-                          </p>
-                          <div>{[<a href='/'>More</a>]}</div>
-                        </li>
-                        <li className="collection-item avatar">
-                          <i className="material-icons circle">attach_money</i>
-                          <span className="title flow-text">Price (COP)</span>
-                          <p>Less than $50000.0 <br></br>
-                              $51000.00 - $500000.0 <br></br>
-                              More than $500000.0
-                          </p>
-                          <div>{[<a href='/'>More</a>]}</div>
-                        </li>
-                      </ul>
-                    </div>
-                </Col>
-            </Row>
-            <div className="container center">
-              <Pagination items={10} activePage={1} maxButtons={8} />
-            </div>
-          </div>
+
+      </div>
     );
   }
 }
