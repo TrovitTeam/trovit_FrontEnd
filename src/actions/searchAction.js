f01c5ce677a884d158e250e6c54166ab994c4872
miacastroco <miacastroco@unal.edu.co>
2018-12-15 13:45:40 -0500

    Update search action

--- src/actions/searchAction.js
+++ src/actions/searchAction.js
@@ -2,12 +2,12 @@ import Axios from "axios";
 import { baseUrl } from '../resources/url';
 import unsplash from '../apis/unsplash';
 
-export const fetchSearchResults = (term) => {
+export const fetchSearchResults = (term) => async (dispatch) => {
     const response = await unsplash.get('/search/photos',{
         params: {query: term}
     });
 
-    dispatchEvent({
+    dispatch({
         type:'FETCH_SEARCH_RESULTS',
         paylod: response
     })
