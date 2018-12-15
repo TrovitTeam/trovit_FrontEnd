ab761a311a20106d1f090a924f99419736f15b6a
miacastroco <miacastroco@unal.edu.co>
2018-12-15 14:48:53 -0500

    Search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
 
   onFormSubmit = (e) => {
     e.preventDefault();
-    
+    this.props.fetchSearchResults(this.state.term);
   }
 
   render() {
--- src/components/Search_result.js
+++ src/components/Search_result.js
@@ -6,39 +6,9 @@ import ProductCard from "./ProductCard";
 import src02 from "../resources/Captura02.PNG";
 
 class Search_result extends Component {
-	componentDidMount = () => {
-		this.props.fetchSearchResults();
-	};
-
-	renderList = () => {
-		return this.props.searchResults.map(result => {
-			return (
-				<ProductCard
-					image={src02}
-					title={this.props.searchResults.title}
-					description={this.props.searchResults.description}
-					actions={[<a href="/">Product Link</a>]}
-				>
-					{result.description}
-				</ProductCard>
-			);
-		});
-	};
-
 	render() {
-		if (!this.props.searchResults) {
-			return <div>No results</div>;
-		}
-
-		return <div className="container">{this.renderList()}</div>;
+		return <div className="container">Search_result</div>;
 	}
 }
 
-const mapStateToProps = state => {
-	return { searchResults: state.props };
-};
-
-export default connect(
-	mapStateToProps,
-	{ fetchSearchResults }
-)(Search_result);
+export default Search_result;
