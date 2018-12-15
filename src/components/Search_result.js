b3687ffe99127c3bdc3d655ec95109d221d62072
miacastroco <miacastroco@unal.edu.co>
2018-12-15 13:52:53 -0500

    Update Search Result

--- src/components/Search_result.js
+++ src/components/Search_result.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import fetchSearchResults from '../actions/searchAction';
-import {Card} from 'react-materialize';
+import {fetchSearchResults} from '../actions/searchAction';
+import {Card, CardTitle} from 'react-materialize';
 import src02 from "../resources/Captura02.PNG";
 
 
 class Search_result extends Component {
 
   componentDidMount = () => {
-    this.props.searchAction();
+    this.props.fetchSearchResults();
   }
 
   renderList = () => {
-    return this.props.fetchSearchResults.map( result => {
+    /* return this.props.searchAction.map( result => {
       return(
         <Card className='small blue-grey'
-          header={<CardTitle image={src02}>{result.title}</CardTitle>}
+          header={<CardTitle image={src02}>{ result.title }</CardTitle>}
           actions={[<a href='/'>Product Link</a>]}>
-          {/*result.description*/}
+          {result.description}
         </Card>
       )
-    })
+    }) */
+    return <div>renderList</div>
   }
 
   render() {
