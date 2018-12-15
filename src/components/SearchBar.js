3da188f9b0282f9c39719e951f317e6e167b962a
miacastroco <miacastroco@unal.edu.co>
2018-12-15 14:14:24 -0500

    Update Search Bar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  state = {
+    term: ' '
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
   render() {
     return (
       <div>
+        <form onSubmit = {this.onFormSubmit}>
+          <div className="input-field">
+            <input type="text" value={this.state.term}/>
+            <label className="label-icon" htmlFor="search"></label>
+            <i className="material-icons">close</i>
+          </div>
+        </form>
       </div>
     );
   }
