74e5774311183f5ab51e0f10537685b5c937b983
Arka Ariza <arjarizami@unal.edu.co>
2019-01-15 23:42:35 -0500

    a

--- src/actions/signinActions.js
+++ src/actions/signinActions.js
@@ -1,31 +1,28 @@
 import axios from "axios";
 import { baseUrl } from "../resources/url.js";
+import history from "../history";
 
-export function userSigninRequest(userData) {
-  return dispatch => {
-    //return axios.post('http://localhost:3000/users', userData);
-    console.log(userData.name);
-    console.log(baseUrl);
-    return axios({
-      method: "post",
-      url: baseUrl + "users",
-      responseType: "json",
-      data: {
-        user: {
-          name: userData.name,
-          location: "Bogota",
-          userType: userData.userType,
-          phone: userData.phone,
-          email: userData.email,
-          password: userData.password
-        }
+export const userSigninRequest = userData => async dispatch => {
+  history.push("/");
+  await axios({
+    method: "post",
+    url: baseUrl + "users",
+    responseType: "json",
+    data: {
+      user: {
+        name: userData.name,
+        location: "Bogota",
+        userType: userData.userType,
+        phone: userData.phone,
+        email: userData.email,
+        password: userData.password
       }
+    }
+  })
+    .then(function(response) {
+      console.log(response);
     })
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  };
-}
+    .catch(function(error) {
+      console.log(error);
+    });
+};
