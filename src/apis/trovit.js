ec0ed35c901789d61c4bc798633574680265b47e
miacastroco <miacastroco@unal.edu.co>
2019-02-01 19:33:24 -0500

    Heroku Ready

--- src/apis/trovit.js
+++ src/apis/trovit.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-/*export default axios.create({
+export default axios.create({
   baseURL: "https://trovit.herokuapp.com"
 });
-*/
-export default axios.create({
+
+/* export default axios.create({
   baseURL: "http://localhost:3000/"
-});
+}); */
